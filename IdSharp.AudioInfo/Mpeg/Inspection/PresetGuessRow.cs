c7c4b45222f904e39befa1413c78a59dfa054799
Randall Flagg <O@G>
2026-09-02 12:23:07 +0100

    Fix CA1051: convert visible instance fields to properties with PascalCase

--- IdSharp.AudioInfo/Mpeg/Inspection/PresetGuessRow.cs
+++ IdSharp.AudioInfo/Mpeg/Inspection/PresetGuessRow.cs
@@ -2,9 +2,9 @@ namespace IdSharp.AudioInfo.Mpeg.Inspection;
 
 internal sealed class PresetGuessRow
 {
-    public LameVersionGroup[] VGs = new LameVersionGroup[3];  // Version Groups
-    public byte[] TVs = new byte[7];                          // Tag Values
-    public LamePreset Res;                                    // Resulting Lame Preset
+    public LameVersionGroup[] Vgs { get; set; } = new LameVersionGroup[3];
+    public byte[] Tvs { get; set; } = new byte[7];
+    public LamePreset Res { get; set; }
 
     public PresetGuessRow(
     byte tv1, byte tv2, byte tv3, byte tv4, byte tv5, byte tv6, byte tv7,
@@ -20,7 +20,7 @@ internal sealed class PresetGuessRow
     {
         for (var i=0; i<3; i++)
         {
-            if (vg1 == VGs[i])
+            if (vg1 == Vgs[i])
             {
                 return true;
             }
@@ -33,16 +33,16 @@ internal sealed class PresetGuessRow
         byte tv1, byte tv2, byte tv3, byte tv4, byte tv5, byte tv6, byte tv7,
         LamePreset result)
     {
-        VGs[0] = vg1;
-        VGs[1] = vg2;
-        VGs[2] = vg3;
-        TVs[0] = tv1;
-        TVs[1] = tv2;
-        TVs[2] = tv3;
-        TVs[3] = tv4;
-        TVs[4] = tv5;
-        TVs[5] = tv6;
-        TVs[6] = tv7;
+        Vgs[0] = vg1;
+        Vgs[1] = vg2;
+        Vgs[2] = vg3;
+        Tvs[0] = tv1;
+        Tvs[1] = tv2;
+        Tvs[2] = tv3;
+        Tvs[3] = tv4;
+        Tvs[4] = tv5;
+        Tvs[5] = tv6;
+        Tvs[6] = tv7;
         Res = result;
     }
 }
